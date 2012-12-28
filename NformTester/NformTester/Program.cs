5798238b72b9294c052c52fcb0bd6118767ebd06
unknown <y93248@.emrsn.org>
2012-12-28 11:19:47 +0800

    optimise detecting available device

--- NformTester/NformTester/Program.cs
+++ NformTester/NformTester/Program.cs
@@ -19,6 +19,7 @@ using Lextm.SharpSnmpLib.Messaging;
 using System.Net;
 using System.Net.Sockets;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 using Ranorex;
 using Ranorex.Core;
@@ -33,31 +34,15 @@ namespace NformTester
         [STAThread]
         public static int Main(string[] args)
         {
-        	//Check SNMP device configured in device.ini is available.
-        	List<string> NotAvailableSNMPDevice = CheckSNMPDeviceAvailable();
-        	if(NotAvailableSNMPDevice!= null && NotAvailableSNMPDevice.Count>0)
+        	if(!CheckDeviceAvailable())
         	{
-        		Console.WriteLine("These devices are not available!");
-        		foreach(string s_IP in NotAvailableSNMPDevice)
-        		{
-        			Console.WriteLine("Unavailable SNMP device is:" + s_IP);
-        		}
-        		MessageBox.Show("Do you want to continue? Click Ok button to continue.");
-        	}
-        	
-        	//Check velocity device configured in device.ini is available.
-        	List<string> NotAvailableVelocityDevice = CheckVelocityDeviceAvailable();
-            if(NotAvailableVelocityDevice!= null && NotAvailableVelocityDevice.Count>0)
-        	{
-        		Console.WriteLine("These devices are not available!");
-        		foreach(string s_IP in NotAvailableVelocityDevice)
-        		{
-        			Console.WriteLine("Unavailable Velocity devices :" + s_IP);
-        		}
-        		MessageBox.Show("Do you want to continue? Click Ok button to continue.");
-        	}
+        		DialogResult dr = MessageBox.Show("Do you want to continue? Click Ok button to continue.", "Warning" , MessageBoxButtons.YesNo,MessageBoxIcon.Warning,MessageBoxDefaultButton.Button2);
+            	if(dr == DialogResult.No)
+            	{
+            		return 0;
+            	}
+        	}                    
             
-        	
         	Keyboard.AbortKey = System.Windows.Forms.Keys.Pause;
             int error = 0;
             try
@@ -72,7 +57,72 @@ namespace NformTester
             return error;
         }
         
-        	//**********************************************************************
+         //**********************************************************************
+		/// <summary>
+		/// Run cmd command
+		/// </summary>
+		public static string RunCommand(string command)
+		{
+			Process p = new Process();
+			p.StartInfo.FileName = "cmd.exe";
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardInput = true;
+			p.StartInfo.RedirectStandardOutput = false;
+			p.StartInfo.RedirectStandardError = true;
+			p.StartInfo.CreateNoWindow = true;
+			p.Start();
+			p.StandardInput.WriteLine(command);
+			p.StandardInput.WriteLine("exit");
+			//string strRst = p.StandardOutput.ReadToEnd();
+			Delay.Duration(10000);
+			return  "";
+		}
+        
+        //**********************************************************************
+		/// <summary>
+		/// Check the device is available or not
+		/// </summary>
+		public static bool CheckDeviceAvailable()
+		{		
+			//stop Nform service
+			Console.WriteLine("Stop Nform service...");
+			string strRst = RunCommand("sc stop Nform");				
+			//string strRst = "";
+			//Check SNMP device configured in device.ini is available.
+			bool result = true;
+        	List<string> NotAvailableSNMPDevice = CheckSNMPDeviceAvailable();
+        	if(NotAvailableSNMPDevice!= null && NotAvailableSNMPDevice.Count>0)
+        	{
+        		Console.WriteLine("These devices are not available!");
+        		foreach(string s_IP in NotAvailableSNMPDevice)
+        		{
+        			Console.WriteLine("Unavailable SNMP device is:" + s_IP);
+        			result = false;
+        		}
+        		//MessageBox.Show("Do you want to continue? Click Ok button to continue.");
+        	}
+        	
+        	//Check velocity device configured in device.ini is available.
+        	List<string> NotAvailableVelocityDevice = CheckVelocityDeviceAvailable();
+            if(NotAvailableVelocityDevice!= null && NotAvailableVelocityDevice.Count>0)
+        	{
+        		Console.WriteLine("These devices are not available!");
+        		foreach(string s_IP in NotAvailableVelocityDevice)
+        		{
+        			Console.WriteLine("Unavailable Velocity devices :" + s_IP);
+        			result = false;
+        		}
+        		//MessageBox.Show("Do you want to continue? Click Ok button to continue.");
+        	}
+            
+            //start Nform service
+            Console.WriteLine("Start Nform service...");
+			strRst = RunCommand("sc start Nform");		
+			
+            return result;
+		}
+        
+        //**********************************************************************
 		/// <summary>
 		/// Check the SNMP devices in the Devices.ini are available or not.
 		/// If some devices are unavailable, the Devices.ini should be changed to give
@@ -129,20 +179,22 @@ namespace NformTester
             IList<Variable> resultdata;
             
             foreach(string deviceIP in keyName){
+            	string strIP = myparseToValue(groupName,deviceIP);
 	            try
-	            {
+	            {	            	
 	            	if(!deviceIP.Equals("GXT_Ip_Port"))
-	            		endpoint.Address = IPAddress.Parse(myparseToValue(groupName,deviceIP));
+	            		endpoint.Address = IPAddress.Parse(strIP);
 	            	else
 	            		endpoint.Address = IPAddress.Parse(IP_Port);
 	            	 resultdata = Messenger.Get(version,endpoint,community,varlist,timeout);
-	            	 data = resultdata[0];
-	            	 Console.WriteLine("Variable data :" + data.Data.ToString() + "in this device:" + deviceIP);
+	            	 data = resultdata[0];	            
+	            	 Console.WriteLine("The device:" + deviceIP + "("+ strIP +")"+ " is availabe");
 	            }
 	            catch(Exception ex)
 	            {
 	            	notAvailable.Add(deviceIP);
-	            	Console.WriteLine("There is no device in this ip address."+ deviceIP +"!" + ex.ToString());
+	            	Console.WriteLine("There is no device in this ip address."+ deviceIP + "("+ strIP +")!" );
+	            	string log = ex.ToString();
 	            	continue;
 	            }
             }
@@ -173,8 +225,10 @@ namespace NformTester
 		    	"VelocitySingleManual_1",
 		    };
 			
-         ClientEngine ve = ClientEngine.GetClientEngine();
-         string GDDpath= @"D:\Velocity";
+         ClientEngine ve = ClientEngine.GetClientEngine();                                                  
+            	
+         string GDDpath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(),
+                                                 "mds");
          int startupflag = ve.Startup(GDDpath);
          Console.WriteLine("GDDPath:" + startupflag);
 	     CommsChannel channel = ve.OpenChannel(CommStack.BACnetIp);
@@ -182,15 +236,16 @@ namespace NformTester
 	     foreach(string deviceIP in keyName){
 		     if(channel != null)
 		     {
-		     	DeviceNode node = channel.StrToEntity(myparseToValue(groupName,deviceIP));
+		     	string strIP = myparseToValue(groupName,deviceIP);
+		     	DeviceNode node = channel.StrToEntity(strIP);
 		     	if(node != null)
 		     	{
-		     		Console.WriteLine("device is available!");
+		     		Console.WriteLine("The device:" + deviceIP + "("+ strIP +")"+ " is availabe");
 		     	}
 		     	else
 		     	{
 		     		notAvailable.Add(deviceIP);
-		     		Console.WriteLine("device is not available!");
+		     		Console.WriteLine("There is no device in this ip address."+ deviceIP + "("+ strIP +")!" );
 		     	}
 		     }
 		     else
